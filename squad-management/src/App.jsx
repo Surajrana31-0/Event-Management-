1638198d3ec1b2e3484880a3c972edcb6ab7ceea
Sydman0258 <iamsid0258@gmail.com>
2025-06-22 20:29:02 +0545

    features

--- squad-management/src/App.jsx
+++ squad-management/src/App.jsx
@@ -4,6 +4,7 @@ import Login from './pages/Authentication/Login.jsx';
 import Register from './pages/Authentication/Register.jsx';
 import Mission from './pages/public/Mission.jsx';
 import './App.css';
+import Feature from "./pages/public/Features.jsx";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
            <Route path="/mission" element={<Mission />} />
+           <Route path="/features" element={<Feature />} />
       </Routes>
     </Router>
   );
